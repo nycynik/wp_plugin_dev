4c43ac801e26bc766d40ab37be98579c78255022
Mike Lynch <nycynik@gmail.com>
2020-07-23 16:20:32 -0700

    adding simple folder plugin

---
+++ folder_plugin/simple-folders.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Simple Folders
+Description: Simple folders for Organizing Media on your site.
+Plugin URI:  https://mikelynchgames.com/simple_folders
+Author:      Mike Lynch
+Version:     2.1
+License:     GPLv3
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version
+2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+with this program. If not, visit: https://www.gnu.org/licenses/
+*/
